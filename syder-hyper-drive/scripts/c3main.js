d6c55bda500294b8c5095d6e19e6987e3fbf4e64
buitrongtuan <154553857+pupuchibu@users.noreply.github.com>
2024-12-14 23:16:08 +0700

    970

---
+++ syder-hyper-drive/scripts/c3main.js
@@ -0,0 +1,9 @@
+import "./../gl-matrix.js";
+import "./../gltfData.js";
+import "./../gltfModel.js";
+import "./../gltfModelW.js";
+import "./../box2d.wasm.js";
+import "./c3runtime.js";
+import "./objRefTable.js";
+import "./project/main.js";
+import "./project/scriptsInEvents.js";
---
+++ traffic-escape-puzzle/index.js
@@ -0,0 +1,6 @@
+window.screenOrientation="portrait",
+loadLib("https://h5gamessdk.yyggames.com/sdk/laya/2.13.1/laya.core.js"),
+loadLib("https://h5gamessdk.yyggames.com/sdk/laya/2.13.1/laya.ui.js"),
+loadLib("https://h5gamessdk.yyggames.com/sdk/laya/2.13.1/laya.d3.js"),
+loadLib("https://h5gamessdk.yyggames.com/sdk/laya/2.13.1/laya.physics3D.js"),
+loadLib("js/main.js");
